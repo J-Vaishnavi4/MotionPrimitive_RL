a7969d4854c163d839065282e58fea2c48614bd0
J-Vaishnavi4 <jvaishnavi4127@gmail.com>
2023-09-16 19:23:58 +0530

    Motion Primitives using RL

---
+++ simple_driving/resources/turtlebot_env.py
@@ -0,0 +1,39 @@
+import pybullet as p
+import os
+import math
+import gym
+import time
+
+class TurtleBot:
+    def __init__(self, client):
+        self.client = client
+        p.connect(p.DIRECT)
+        p.setGravity(0,0,-10)
+        offset = [0,0,0]
+        self.turtle = p.loadURDF('/home/vaishnavi/Documents/IISc/Car-Plane robot_RL/Gym-Medium-Post/turtlebot3_description/urdf/turtlebot3_burger.urdf.xacro',offset)
+        self.plane = p.loadURDF('/home/vaishnavi/Documents/IISc/Car-Plane robot_RL/Gym-Medium-Post/simple_driving/resources/simpleplane.urdf')
+        self.wheel_joints = [1, 2]
+        # Joint speed
+        self.joint_speed = 0
+
+    def get_ids(self):
+        return self.turtle, self.client
+
+    def apply_action(self, action):
+        # Expects action to be two dimensional
+        leftWheelVelocity,rightWheelVelocity = action[0],action[1]
+        p.setJointMotorControlArray(self.turtle,[1,2],p.VELOCITY_CONTROL, targetVelocities=[leftWheelVelocity,rightWheelVelocity],forces=[1000,1000])
+
+    def get_observation(self):
+        # Get the position and orientation of the car in the simulation
+        pos, ang = p.getBasePositionAndOrientation(self.turtle, self.client)
+        ang = p.getEulerFromQuaternion(ang)
+        ori = (math.cos(ang[2]), math.sin(ang[2]))
+        pos = pos[:2]                                                       # Position- X, Y
+        # Get the velocity of the car
+        vel = p.getBaseVelocity(self.turtle, self.client)[0][0:2]           # Velocities- Vx, Vy
+        # Concatenate position, orientation, velocity
+        observation = (pos + ori + vel)
+        # print("obs: ", observation)
+
+        return observation
---
+++ turtlebot3_description/urdf/control_bot.py
@@ -0,0 +1,32 @@
+import pybullet as p
+import time
+import math
+
+p.connect(p.GUI)
+p.setGravity(0,0,-10)
+offset = [0,0,0]
+
+turtle = p.loadURDF('/home/vaishnavi/Documents/IISc/Car-Plane robot_RL/Gym-Medium-Post/turtlebot3_description/urdf/turtlebot3_burger.urdf.xacro',offset)
+plane = p.loadURDF('/home/vaishnavi/Documents/IISc/Car-Plane robot_RL/Gym-Medium-Post/simple_driving/resources/simpleplane.urdf')
+p.setRealTimeSimulation(1)
+
+for j in range (p.getNumJoints(turtle)):
+	print(p.getJointInfo(turtle,j))
+
+def apply_action(action):
+        # Expects action to be two dimensional
+        leftWheelVelocity,rightWheelVelocity = action[0],action[1]
+        p.setJointMotorControlArray(turtle,[1,2],p.VELOCITY_CONTROL, targetVelocities=[leftWheelVelocity,rightWheelVelocity],forces=[10,10])
+        vel = p.getBaseVelocity(turtle)[0]
+        print("Vel: ",leftWheelVelocity, rightWheelVelocity,vel)
+        # p.setJointMotorControl2(turtle,2,p.VELOCITY_CONTROL,targetVelocity=rightWheelVelocity,force=1000)
+        # p.setJointMotorControl2(turtle,1,p.VELOCITY_CONTROL,targetVelocity=leftWheelVelocity,force=1000)
+leftWheelVelocity = 0
+rightWheelVelocity = 0
+while(1):
+        time.sleep(1./240.)
+        p.stepSimulation()
+        for i in range(4):
+                print("i: ",i)
+                action = [100*math.cos(i/2),100*math.cos(i/2)]
+                apply_action(action)
